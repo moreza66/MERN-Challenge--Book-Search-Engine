853f12ada228477f0d22777efd1c7141f1aebc98
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-11 19:08:18 -0600

    adding a header and make sure it's running on the app.js

---
+++ client/src/pages/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+export default function Header() {
+
+
+    return (
+
+        <div className="w-full bg-gray-100 mb-8">
+            <div className="container text-center py-4">
+    
+            </div>
+            <div className="flex justify-center">
+                    testing ittttt
+            </div>
+        </div>
+    )
+}

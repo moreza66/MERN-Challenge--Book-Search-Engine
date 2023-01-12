43599692aca4370538c970a1159c52d66d70b9e1
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-12 00:07:54 -0600

    adding user routes

---
+++ server/routes/books.js
@@ -0,0 +1,195 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Array,
+        required: true
+    }
+})
+
+const Users = mongoose.model('users', userSchema)
+
+module.exports = Usersconst express = require('express')
+const router = express.Router()
+const Users = require('../models/users')
+const Books = require('../models/books')
+const jwt = require('jsonwebtoken')
+const { check } = require('express-validator');
+const { isAuth, isAdmin } = require('../middlewares/authMiddleware')
+var multer = require('multer');
+const fs = require('fs')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+})
+
+var upload = multer({ storage: storage });
+
+router.post(
+    '/new',
+    [
+        check('title').escape().trim(),
+        check('author').escape().trim(),
+        check('description').escape().trim(),
+    ],
+    isAuth,
+    isAdmin,
+    upload.single('image'),
+    async (req, res) => {
+        try {
+            const token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const user = await Users.findById(decoded.id)
+            if (user.role.includes('admin') === false) {
+                res.status(401).send({
+                    message: 'Unauthorized'
+                })
+                return
+            }
+
+            const { title, author, description } = req.body
+            const newBook = await Books.create({
+                title,
+                author,
+                description,
+                image: req.file.path
+            })
+            res.status(200).json(newBook)
+            return
+        }
+        catch (err) {
+            res.send(err)
+        }
+    },
+)
+
+router.get('/all', async (req, res) => {
+    try {
+        const books = await Books.find({})
+        res.status(200).json(books)
+        return
+    }
+    catch (err) {
+        res.status(500)
+            .send({
+                message: err.message || "Some error occurred while retrieving books."
+            })
+    }
+})
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Books.findById(req.params.id)
+        res.status(200).json(book)
+        return
+    }
+    catch (err) {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving books."
+        })
+    }
+})
+
+
+//edit book
+router.put('/edit/:id', [
+    check('title').escape().trim(),
+    check('author').escape().trim(),
+    check('description').escape().trim(),
+], isAuth, isAdmin,
+    upload.single('image')
+    , async (req, res) => {
+        try {
+
+            if (req.file === undefined) {
+                const { title, author, description } = req.body
+                const book = await Books.findByIdAndUpdate(req.params.id, {
+                    title: title,
+                    author: author,
+                    description: description,
+                })
+                res.status(200).json(book)
+                return
+            }
+
+
+            const oldImage = await Books.findById(req.params.id)
+            fs.unlink(oldImage.image, (err) => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+            })
+            const { title, author, description } = req.body
+            const book = await Books.findByIdAndUpdate(req.params.id, {
+                title: title,
+                author: author,
+                description: description,
+                image: req.file.path
+            })
+
+
+            res.status(200).json(book)
+            return
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving books."
+            })
+        }
+    })
+
+
+router.delete('/delete/:id', isAuth, isAdmin, async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const book = await Books.findByIdAndDelete(req.params.id)
+        res.status(200).json(book)
+        return
+    }
+    catch (err) {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving books."
+        })
+    }
+})
+
+//search book
+router.get('/search/:title', isAuth, async (req, res) => {
+    try {
+        if (req.params.title === '') {
+            const books = await Books.find({})
+            res.status(200).json(books)
+            return
+        }
+
+        const books = await Books.find({ title: { $regex: req.params.title, $options: 'i' } })
+        res.status(200).json(books)
+        return
+    }
+    catch (err) {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving books."
+        })
+    }
+})
+
+
+module.exports = router
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,21 +1,187 @@
-const mongoose = require('mongoose')
+const express = require('express')
+const router = express.Router()
+const Users = require('../models/users')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const env = require('dotenv').config()
+const { check, validationResult } = require('express-validator');
+const { isAuth, isAdmin } = require('../middlewares/authMiddleware')
+const Favorites = require('../models/favorites')
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: Array,
-        required: true
+//login
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const user = await Users.findOne({ email })
+        if (!user) {
+            res.status(400).json({ message: 'User not found' })
+            return
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            res.status(400).json({ message: 'Invalid credentials' })
+            return
+        }
+        res.status(200).send({
+            token: jwt.sign({
+                id: user._id,
+                role: user.role
+            }, process.env.JWT_SECRET, {
+                expiresIn: '30d'
+            })
+        })
+        return
+    } catch (err) {
+        res.json({
+            message: err
+        })
     }
 })
 
-const Users = mongoose.model('users', userSchema)
+//register
+router.post('/register', [
+    check('email').isEmail().normalizeEmail().withMessage('Please enter a valid email'),
+], async (req, res) => {
+    const validationResults = validationResult(req)
+    if (!validationResults.isEmpty()) {
+        return res.status(400).json({
+            message: validationResults.errors[0].msg
+        })
+    }
+    try {
+        const { email, password } = req.body
+        const userExists = await Users.findOne({
+            email
+        })
+        if (userExists) {
+            res.status(400).send({
+                message: 'User already exists'
+            })
+            return
+        }
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if (err) throw err
+                const newUser = await Users.create({
+                    email,
+                    password: hash,
+                    role: ['user']
+                })
+                res.json({
+                    token: jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+                        expiresIn: '30d'
+                    })
+                })
+            })
+        })
 
-module.exports = Users
+    } catch (err) {
+        res.json({
+            message: err
+        })
+    }
+})
+
+//add to favorites
+router.post('/addFavorite/:id', [
+    check('bookID').isMongoId(),
+    check('userId').isMongoId()
+], isAuth, async (req, res) => {
+
+    try {
+        const { id } = req.params
+
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decoded.id
+
+        //check if book is already in favorites
+        const favorite = await Favorites.findOne({
+            userID: userId,
+            bookID: id
+        })
+        if (favorite) {
+            res.status(400).json({
+                message: 'Book already in favorites'
+            })
+            return
+        }
+
+        //inset to favorites
+        await Favorites.create({
+            userID: userId,
+            bookID: id
+        })
+
+        res.status(200).json({
+            message: 'Added to favorites'
+        })
+    } catch (err) {
+        res.json({
+            message: err.message
+        })
+        return
+    }
+})
+
+//remove from favorites
+router.post('/removeFavorite/:id', isAuth, async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decoded.id
+
+        //check if book is already in favorites
+        const favorite = await Favorites.findOne({
+            userId,
+            bookID: id
+        })
+        if (!favorite) {
+            res.status(400).json({
+                message: 'Book not in favorites'
+            })
+            return
+        }
+
+        //remove from favorites
+        await Favorites.deleteOne({
+            userId,
+            bookID: id
+        })
+
+        res.status(200).json({
+            message: 'Removed from favorites'
+        })
+    } catch (err) {
+        res.json({
+            message: err.message
+        })
+        return
+    }
+})
+
+router.get('/favorites', isAuth, async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decoded.id
+
+        const favorites = await Favorites.find({
+            userID: userId
+        })
+
+        res.status(200).json({
+            favorites
+        })
+    } catch (err) {
+        res.json({
+            message: err.message
+        })
+        return
+    }
+})
+module.exports = router
+

466cd43d75fc15fe699528e4b8cfa39ed210f7a6
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-11 23:28:10 -0600

    import the header to navbar

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -5,13 +5,12 @@ export default function Header() {
 
 
     return (
-
         <div className="w-full bg-gray-100 mb-8">
             <div className="container text-center py-4">
-    
+                <Link to="/">Search Book</Link>
             </div>
             <div className="flex justify-center">
-                    testing ittttt
+                <Navbar />
             </div>
         </div>
     )

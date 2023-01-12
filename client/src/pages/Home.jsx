a5a050ab74b2c3a969efc5a97dc580b5dff4555b
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-11 22:44:29 -0600

    start working on home components

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import Form from 'react-bootstrap/Form';
+import axios from "axios";
+import getToken from "../hooks/getToken";
+import useLoggedin from "../hooks/useLoggedin";
+
+export default function Home() {
+    
+}
+return

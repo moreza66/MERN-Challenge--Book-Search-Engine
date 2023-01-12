cdb5180be9ecc0b80be9393eed8a7f291168855b
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-11 19:16:06 -0600

    adding a footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import Footer from "./pages/Footer";
 import Header from "./pages/Header";
 
 function App() {
   return (
+   <div>
     <Header/>
+    <Footer/>
+   </div>
+  
+    
+
   )
 
+
+
 }
+
 
 export default App;
---
+++ client/src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className="w-full bg-gray-100 mt-8">
+            <div className="container text-center py-4">
+                Footer
+            </div>
+        </div>
+    )
+}

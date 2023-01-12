4ebebdc2b1e4bb67d585e9ae08058c2cc77315fb
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-12 00:00:23 -0600

    adding users models

---
+++ server/models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Array,
+        required: true
+    }
+})
+
+const Users = mongoose.model('users', userSchema)
+
+module.exports = Users
